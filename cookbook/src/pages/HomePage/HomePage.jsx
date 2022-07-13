6ba0bd9354269c80ba3ce3a10fd4560869a8ee31
NikolaV91 <vasic.nikol@outlook.com>
2022-07-13 13:30:39 +0200

    update on design of homepage

--- cookbook/src/pages/HomePage/HomePage.jsx
+++ cookbook/src/pages/HomePage/HomePage.jsx
@@ -12,63 +12,54 @@ import ConfirmModal from '../../components/confirmModal/ConfirmModal';
 
 const HomePage = (props) => {
     const [search, setSearch] = useState("")
-    
-
 
     return (
         <div className='homewrap'>
 
             <a href="#topPage" id='backOnTopLink'></a>
-           
-            {props.confirmModal === "open" ? <ConfirmModal selectedRecipeId={props.selectedRecipeId} setShouldUpdate={props.setShouldUpdate} recipe={props.recipe} changeConfirmModal={props.changeConfirmModal}/> : null}
+
+            {props.confirmModal === "open" ? <ConfirmModal selectedRecipeId={props.selectedRecipeId} setShouldUpdate={props.setShouldUpdate} recipe={props.recipe} changeConfirmModal={props.changeConfirmModal} /> : null}
             <div className='main'>
-          
-                <p className='maintext'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, cum?</p>
+
+                <p className='mainText'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-
-
-
-
-
-            <div className='secmain01'>
-                <div className='secmain001'>
-                    <img src={artist} alt="artist" className='artistimg' />
-                    <img src={artist2} alt="artist2" className='artistimg' />
-                    <div id='about' className='secmain0001'>
-                        <p className='t11'>WHO IS THE ASTIST?</p>
-                        <p className='t12'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid assumenda consectetur beatae asperiores, recusandae rem aspernatur dolore tempore repudiandae nostrum dolores itaque nam! Tenetur delectus autem eius sunt dolores? Molestias!</p>
-                        <p className='t12'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est eius assumenda at quos facilis sit sequi ipsam cumque neque quidem iusto non, vitae quod necessitatibus, dolore deserunt voluptas iste perferendis, distinctio qui repellendus vel doloribus dicta. Officia, expedita dignissimos?</p>
-                        <p className='t12'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid assumenda consectetur beatae asperiores, recusandae rem aspernatur dolore tempore repudiandae nostrum dolores itaque nam! Tenetur delectus autem eius sunt dolores? Molestias!</p>
+            <div className='section_01'>
+                <div className='div_sec01_wrapper'>
+                    <img src={artist} alt="artist" className='artistImg' />
+                    <img src={artist2} alt="artist2" className='artistImg' />
+                    <div id='about' className='div_sec01_wrapper_artist'>
+                        <p className='artistText_01'>WHO IS THE ASTIST?</p>
+                        <p className='artistText_02'>Lorem e nostrum dolores itaque nam! Tenetur delectus autem eius sunt dolores? Molestias!</p>
+                        <p className='artistText_02'>Lorem a at quos facilis sit sequi ipsam cumque neque quidem iusto non, vitae quod necessitatibus, dolore deserunt voluptas iste perferendis, distinctio qui repellendus vel doloribus dicta. Officia, expedita dignissimos?</p>
                     </div>
                 </div>
             </div>
 
             {/* valjda je ovde kraj */}
-            <div className='secmain'>
-                <div id='history' className='secmain1'>
-                    <p className='t1'>HISTORY OF THE PERFORMER</p>
-                    <p className='t2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, ipsa fugit? Dicta labore cupiditate dolore nesciunt excepturi. Ipsam, voluptates voluptatibus.</p>
-                   
+            <div className='section_02'>
+                <div id='history' className='div_sec02_history_wrapper'>
+                    <p className='artistText_01'>HISTORY OF THE PERFORMER</p>
+                    <p className='artistText_02'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, ipsa fugit? Dicta labore cupiditate dolore nesciunt excepturi. Ipsam, voluptates voluptatibus.</p>
                 </div>
 
-                <div className='secmain2'>
-                    <div className='sm21'>
+                <div className='div_sec02_food_wrapper'>
+                    <div className='div_food_wrapper_01'>
                         <img src={pies} alt="burger" className='p1' />
-                        <p className='littletitle'>FOOD</p>
+                        <p className='foodTitle'>FOOD</p>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt voluptas eum iure quas odio ipsum distinctio cumque ut modi. Sequi, ipsa repellendus quae tempore debitis ratione molestias ut dicta, aliquid iure earum placeat optio tenetur provident voluptatibus reprehenderit itaque.</p>
                     </div>
 
-                    <div className='sm21'>
+                    <div className='div_food_wrapper_01'>
                         <img src={soup} alt="salad" className='p1' />
-                        <p className='littletitle'>FOOD</p>
+                        <p className='foodTitle'>FOOD</p>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt voluptas eum iure quas odio ipsum distinctio cumque ut modi. Sequi, ipsa repellendus quae tempore debitis ratione molestias ut dicta, aliquid iure earum placeat optio tenetur provident voluptatibus reprehenderit itaque.</p>
                     </div>
 
-                    <div className='sm21'>
+                    <div className='div_food_wrapper_01'>
                         <img src={oatmeal} alt="pancakes" className='p1' />
-                        <p className='littletitle'>FOOD</p>
+                        <p className='foodTitle'>FOOD</p>
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt voluptas eum iure quas odio ipsum distinctio cumque ut modi. Sequi, ipsa repellendus quae tempore debitis ratione molestias ut dicta, aliquid iure earum placeat optio tenetur provident voluptatibus reprehenderit itaque.</p>
                     </div>
@@ -77,13 +68,13 @@ const HomePage = (props) => {
             </div>
 
             {/* SKILLS PART */}
-            <div id='skills' className='skilldiv'>
-                <div className='pictureskill'></div>
-                <div className='skillnote'>
+            <div id='skills' className='section_03'>
+                <div className='div_sec03_picture'></div>
+                <div className='div_sec03_note'>
                     <p className='title'>SKILLS</p>
                     <p className='comment'>~Everything You Want To Know~</p>
                     <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo animi repellendus provident perspiciatis magnam eos obcaecati sunt fugit, unde veritatis error numquam et, nisi aspernatur aut. Consectetur deserunt exercitationem suscipit.</p>
-                    <div className='skillwrap'>
+                    <div className='skillsWrapper'>
 
                         <div className='skill'>
                             <p>Creativity</p>
@@ -101,14 +92,7 @@ const HomePage = (props) => {
                             <p>Decision making</p>
                             <p>95%</p>
                         </div>
-                        <div className='skill'>
-                            <p>Artistic aptitude</p>
-                            <p>88%</p>
-                        </div>
-                        <div className='skill'>
-                            <p>Project management</p>
-                            <p>90%</p>
-                        </div>
+
 
                     </div>
                 </div>
@@ -117,22 +101,17 @@ const HomePage = (props) => {
 
             {/* DEO ZA SEARCH I CREATE BTN */}
 
-            <div className='secheader'>
-                <div className='specwrap'>
-                    <div><p className='srchTitle'>Make magic with the artist! Share your favorites!</p></div>
-                    <div className='secwrap'>
-
-                        <div className='macaroondiv'>
-                            <div className='macpicdiv'></div>
-                        </div>
-
+            <div className='section_04'>
+                <div className='div_sec04_wrapper'>
+                    <div><p id='dishes' className='searchTitle'>Make magic with the artist! Share your favorites!</p></div>
+                    <div className='div_sec04_wrapper_btn'>
                         <div className='search-box'>
-                            <button className="btn-search"><i className="fas fa-search"></i></button>
                             <input type="text" className="input-search" placeholder="Type to Search..." onChange={(e) => setSearch(e.target.value)} />
+                            <button className="btn-search"></button>
                         </div>
-                        <div className='lastwrap'>
-                            <button className='addbtn' onClick={() => props.setModal(true)}>
-                                <p className='btntext'>Create your recipe</p>
+                        <div className='createBtn-wrapper'>
+                            <button className='addBtn' onClick={() => props.setModal(true)}>
+                                Create new recipe
                             </button>
                         </div>
                     </div>
@@ -142,10 +121,10 @@ const HomePage = (props) => {
 
             {/* CARDS PART */}
 
-            <div className='cardwrap'>
-                <div className='cardwrapblur'>
-                    <p id='dishes' className='mealtitle mealtitle1'>READY-TO-EAT DISHES</p>
-                    <div className='divcard'>
+            <div className='section_05'>
+                <div className='div_sec05_card_wrapper_01'>
+                    <h3 >READY-TO-EAT DISHES</h3>
+                    <div className='foodCard'>
                         {props.recipes.filter((e) => {
                             if (search === "" && e.level <= 2) {
                                 return e;
@@ -155,9 +134,10 @@ const HomePage = (props) => {
 
                         }).map(e => <Card changeConfirmModal={props.changeConfirmModal} key={e.id} setShouldUpdate={props.setShouldUpdate} setSelectedRecipe={props.setSelectedRecipe} obj={e} />)}
                     </div>
-
-                    <p className='mealtitle'>EASY TO PREPARE MEALS</p>
-                    <div className='divcard'>
+                </div>
+                <div className='div_sec05_card_wrapper_01'>
+                    <h3 >EASY TO PREPARE MEALS</h3>
+                    <div className='foodCard'>
                         {props.recipes.filter((e) => {
                             if (search === "" && e.level <= 4 && e.level > 2) {
                                 return e;
@@ -167,23 +147,22 @@ const HomePage = (props) => {
 
                         }).map(e => <Card changeConfirmModal={props.changeConfirmModal} key={e.id} setShouldUpdate={props.setShouldUpdate} setSelectedRecipe={props.setSelectedRecipe} obj={e} />)}
                     </div>
-
-                    <p className='mealtitle'>COMPLEX DISHES TO MAKE</p>
-                    <div className='divcard'>
+                </div>
+                <div className='div_sec05_card_wrapper_01'>
+                    <h3>COMPLEX DISHES TO MAKE</h3>
+                    <div className='foodCard'>
                         {props.recipes.filter((e) => {
                             if (search === "" && e.level > 4) {
                                 return e;
                             } else if (e.meal.toLowerCase().includes(search.toLowerCase()) && e.level > 4) {
                                 return e;
                             } else return null;
-
+                            
                         }).map(e => <Card changeConfirmModal={props.changeConfirmModal} key={e.id} setShouldUpdate={props.setShouldUpdate} setSelectedRecipe={props.setSelectedRecipe} obj={e} />)}
                     </div>
                 </div>
+                </div>
             </div>
-
-          
-        </div>
     );
 };
 
